cdc3fedaca3879bb7fc9fa7d315efd300464f988
Moisés <moisesjbc@gmail.com>
2025-01-25 15:07:20 +0000

    Add pipes

--- godot-project/gameplay/bubble/bubble.gd
+++ godot-project/gameplay/bubble/bubble.gd
@@ -2,6 +2,17 @@ extends KinematicBody2D
 
 export var speed: int = 200
 export var velocity: Vector2 = Vector2(1.0, 0.0)
+var trapped_enemy = null
+
 
 func _process(delta):
-	move_and_collide(speed * velocity.rotated(rotation) * delta)
+	var collision = move_and_collide(speed * velocity.rotated(rotation) * delta)
+	if collision:
+		if collision.collider.is_in_group("enemies") and not trapped_enemy:
+			trapped_enemy = collision.collider
+			trapped_enemy.get_parent().remove_child(trapped_enemy)
+		elif collision.collider.is_in_group("pipes"):
+			if trapped_enemy:
+				get_parent().add_child(trapped_enemy)
+				trapped_enemy.global_position = global_position
+			queue_free()
---
+++ godot-project/gameplay/pipes/pipes.gd
@@ -0,0 +1,5 @@
+extends TileMap
+
+
+func _ready():
+	var tile_pos = map_to_world(Vector2(3,1))

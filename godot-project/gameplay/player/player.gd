4d4b34ae3ccf09f0858e90dcd3eeb9163eb30b8a
Moisés <moisesjbc@gmail.com>
2025-01-25 19:44:22 +0000

    Add idle animations

--- godot-project/gameplay/player/player.gd
+++ godot-project/gameplay/player/player.gd
@@ -5,6 +5,7 @@ export var speed: int = 500
 var bubble_scene = preload("res://gameplay/bubble/bubble.tscn")
 var n_bubbles = 1
 signal player_died
+var lateral_animation_threshold = 150
 
 
 func process_movement(delta):
@@ -26,14 +27,26 @@ func process_movement(delta):
 
 
 func process_shooting():
-	look_at(get_global_mouse_position())
+	$bubble_respawn.look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("ui_shoot") and n_bubbles > 0:
 		n_bubbles -= 1
 		var bubble = bubble_scene.instance()
 		get_parent().add_child(bubble)
-		bubble.global_rotation = global_rotation
+		bubble.global_rotation = $bubble_respawn.global_rotation
 		bubble.global_position = $bubble_respawn.global_position
+		
+
+func process_animation():
+	if get_local_mouse_position().y < lateral_animation_threshold and get_local_mouse_position().y > -lateral_animation_threshold:
+		if $sprite.animation != "lateral_idle":
+			$sprite.play("lateral_idle")
+		$sprite.flip_h = get_local_mouse_position().x < 0
+	else:
+		if get_local_mouse_position().y > lateral_animation_threshold:
+			$sprite.play("down_idle")
+		elif get_local_mouse_position().y < -lateral_animation_threshold:
+			$sprite.play("up_idle")
 
 
 func recharge_bubbles():
@@ -42,6 +55,7 @@ func recharge_bubbles():
 
 func _process(delta):
 	process_movement(delta)
+	process_animation()
 	process_shooting()
 
 

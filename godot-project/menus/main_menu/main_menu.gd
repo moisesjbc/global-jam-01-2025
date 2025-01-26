6cf19a0759982afcd87709693601bb292709af27
Moisés <moisesjbc@gmail.com>
2025-01-26 15:31:34 +0000

    Add exit button to main menu

--- godot-project/menus/main_menu/main_menu.gd
+++ godot-project/menus/main_menu/main_menu.gd
@@ -3,7 +3,12 @@ extends Control
 
 func _ready():
 	get_node("/root/music").play_main_menu_music()
+	$center_container/vbox_container/exit_button.visible = (OS.get_name() != "HTML5")
 
 
 func _on_play_button_pressed():
 	get_tree().change_scene("res://gameplay/main/main.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()

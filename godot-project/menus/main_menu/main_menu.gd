12ecee8de173824efa15dffc67256e7541570a1b
Moisés <moisesjbc@gmail.com>
2025-01-26 16:27:56 +0000

    Add styles to main menu and game over

--- godot-project/menus/main_menu/main_menu.gd
+++ godot-project/menus/main_menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _ready():
 	get_node("/root/music").play_main_menu_music()
-	$center_container/vbox_container/exit_button.visible = (OS.get_name() != "HTML5")
+	$exit_button.visible = (OS.get_name() != "HTML5")
 
 
 func _on_play_button_pressed():
